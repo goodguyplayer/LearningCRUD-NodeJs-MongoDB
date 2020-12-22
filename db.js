84cdeafae2e49459a8caff28de7509186c1b92e2
goodguyplayer <42227590+goodguyplayer@users.noreply.github.com>
2020-12-22 11:32:14 -0300

    Followed tutorial to page 2
    Includes pagination

--- db.js
+++ db.js
@@ -2,5 +2,40 @@ const mongoClient = require("mongodb").MongoClient;
 mongoClient.connect("mongodb://localhost", { useUnifiedTopology: true })
             .then(conn => global.conn = conn.db("workshoptdc"))
             .catch(err => console.log(err))
- 
-module.exports = { }
+
+
+const TAMANHO_PAGINA = 5; 
+
+module.exports = { findAll, insertOne, findOne, update, deleteOne, countAll, TAMANHO_PAGINA}
+
+
+function findAll(pagina){ 
+    const tamanhoSkip = TAMANHO_PAGINA * (pagina - 1); 
+    return global.conn.collection("customers")
+                        .find({}).
+                        skip(tamanhoSkip)
+                        .limit(TAMANHO_PAGINA)
+                        .toArray(); 
+}
+
+function insertOne(customer, callback){
+    global.conn.collection("customers").insert(customer, callback);
+}
+
+var ObjectId = require("mongodb").ObjectId;
+function findOne(id, callback){  
+    global.conn.collection("customers").find(new ObjectId(id)).toArray(callback);
+}
+	
+function update(id, customer, callback){
+    global.conn.collection("customers").updateOne({_id: new ObjectId(id)}, {$set: customer}, callback);
+}
+
+function deleteOne(id, callback){
+    global.conn.collection("customers").deleteOne({_id: new ObjectId(id)}, callback);
+}
+
+//callback deve considerar error e count
+function countAll(){  
+    return global.conn.collection("customers").countDocuments();
+}
